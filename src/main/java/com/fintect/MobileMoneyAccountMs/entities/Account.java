d36b977caef00f25ef7214d6c30d162c0dd553e7
samseyi672 <samseyi672@gmail.com>
2024-10-18 06:31:23 +0100

    commits

--- src/main/java/com/fintect/MobileMoneyAccountMs/entities/Account.java
+++ src/main/java/com/fintect/MobileMoneyAccountMs/entities/Account.java
@@ -3,6 +3,7 @@ package com.fintect.MobileMoneyAccountMs.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
@@ -13,6 +14,7 @@ import java.math.BigDecimal;
 @Entity
 //@Getter
 //@Setter
+@Table(name="accounts")
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
